da481d57c3e07ad91e4e7ac633ff8a73cd4a26cb
NailMan <nailalex@gmail.com>
2018-03-05 15:03:17 +0300

    Version 1.1
    Version 1.1
    Full worked version
    + example

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 SheduledEvent
 =====================
 
-Sheduled cycled and runonce events library for Arduino with ZeroPoint time correct
+Sheduled cycled and runonce events library for Arduino with ZeroPoint time correct processing
 
 by Niskorodnov Alexander aka NailAlex(NailMan)
 
@@ -9,11 +9,24 @@ DOWNLOAD
 =====================
 
 Download the latest version (version 1.1) here :
-
-
+https://github.com/NailAlex/SheduledEvent
 
 
 HOW TO USE
 =====================
 
+- Create Event instance
+- Create Event handler for callback
+- Setup event for your needs (RunOnce, RunOnce+Delay, Cycle, Cycle+Delay)
+- Start it!...and update
+
+See simple uncluded example!
+
+- Also you can use the time checking function with the correct processing of the transition through zero (~ 70 day work period)
+
+bool CheckTime(unsigned long lastt, unsigned long idelay);
+
+lastt - Time value (millis) at start position
+idelay - checked pause for event
+Result - if current time > Last Time + Delay (with ZeroPoint correct) then return "True"
 
--- examples/SheduledEventExample/SheduledEventExample.ino
+++ examples/SheduledEventExample/SheduledEventExample.ino
@@ -2,12 +2,11 @@
  DESCRIPTION
  ====================
  Simple example of the SheduledEvent library that run three Events with these parameters:
- - RunOnce Event with delayed start(after 15 sec from start)
+ - RunOnce Event with delayed start(after 10 sec from start)
  - Cycled Event for render to serial port (1 time per second)
- - Cycled and delayed Event(run every 0.5 second after 10 second from start)
+ - Cycled and delayed Event(run every 0.5 second after 15 second from start)
 */ 
 
-#include <SheduledEvent.h>
 #include <SheduledEvent.h>
 
 SheduledEvent uEvent1,uEvent2,uEvent3;
@@ -16,14 +15,16 @@ bool flag = false;
 
 void setup() {
  Serial.begin(9600);
- pinMode(13,OUTPUT);
- digitalWrite(13,LOW);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+ Serial.print("Start all Events"); 
+ 
  //Set RunOnce event with 15 sec delay from start
- uEvent1.SetEvent(EVENT_SEC * 15, 0, Event1_handler, EVENT_RUNONCE | EVENT_DELAYED);
+ uEvent1.SetEvent(EVENT_SEC * 10, 0, Event1_handler, EVENT_RUNONCE | EVENT_DELAYED);
  //Set event for render data to serial with 1 sec cycle
  uEvent2.SetEvent(0, EVENT_SEC, Event2_handler, EVENT_CYCLED);
  //Set cycled and delayed event with 0.5s cycle and 10sec delay 
- uEvent2.SetEvent(EVENT_SEC * 10, 500, Event3_handler, EVENT_CYCLED | EVENT_DELAYED);
+ uEvent3.SetEvent(EVENT_SEC * 15, 500, Event3_handler, EVENT_CYCLED | EVENT_DELAYED);
 
   //Start all Events!
  uEvent1.Start();
@@ -35,15 +36,13 @@ void Event1_handler(){
  Serial.println("This is RunOnce Event! It should be only one!");
 }
 
-
 void Event2_handler(){
  int val = analogRead(A0); 
- Serial.print("This is Cycled Event! A0 value = "); Serial.println(val); 
+ Serial.print("This is Cycled Event! time from start = "); Serial.println(millis()); 
 }
 
 void Event3_handler(){
- int val = analogRead(A1);  
-   Serial.print("This is fast cycled and delayed Event! A1 value = "); Serial.println(val); 
+   Serial.print("This is fast cycled and delayed Event! A1 value = "); Serial.println(analogRead(A0)); 
      if (flag) digitalWrite(13,HIGH); else digitalWrite(13,LOW);
  flag=!flag;
 }
